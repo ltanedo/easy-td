61f606e2f92cd7f8c5efd27d07bf09286ccd0a9b
ltanedo <lloydtan@buffalo.edu>
2022-02-25 19:06:55 +0000

    repair undo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'easy_td',         # How you named your package folder (MyLib)
-  packages = ['__endpoints__','__utility__'],   # Chose the same as "name"
+  packages = ['easy_td'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A declarative library for making the TD Ameritrade API more accessible',   # Give a short description about your library
